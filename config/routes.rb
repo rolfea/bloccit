78520b9a8df7b512a48b8809d4c93316003e9caa
Alex Rolfe <rolfepercussion@gmail.com>
2015-12-19 17:15:11 -0600

    fixed topic problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
-  get 'sponsored_posts_controller/show'
-
-  get 'sponsored_posts_controller/new'
-
-  get 'sponsored_posts_controller/edit'
-
-  get 'show/new'
-
-  get 'show/edit'
-
   resources :topics do
-    resources :posts, :sponsored_posts except: [:index]
+    resources :posts, except: [:index]
+    resources :sponsored_posts, except: [:index]
   end
 
   get 'about' => 'welcome#about'
