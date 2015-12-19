6bb43dcb5f4ce3f7d4ec800a721757f61d707206
Alex Rolfe <rolfepercussion@gmail.com>
2015-12-19 17:05:42 -0600

    not finished yet

---
+++ app/controllers/sponsored_posts_controller_controller.rb
@@ -0,0 +1,11 @@
+class SponsoredPostsControllerController < ApplicationController
+  def show
+    @sponsored_post = SponsoredPost.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'sponsored_posts_controller/show'
+
+  get 'sponsored_posts_controller/new'
+
+  get 'sponsored_posts_controller/edit'
+
+  get 'show/new'
+
+  get 'show/edit'
+
   resources :topics do
-    resources :posts, except: [:index]
+    resources :posts, :sponsored_posts except: [:index]
   end
 
   get 'about' => 'welcome#about'
