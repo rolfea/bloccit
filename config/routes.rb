8b8b538663743ee4bfaea0e0a92b64bce61965f9
Alex Rolfe <rolfepercussion@gmail.com>
2016-01-04 14:31:26 -0600

    checkpoint41 completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
-  resources :users, only: [:new, :create]
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
 
+  resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
-  it { should have_many(:posts)}
+  it { should have_many(:posts) }
+  it { should have_many(:comments) }
   # Test for name
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(1) }
@@ -68,7 +69,7 @@ RSpec.describe User, type: :model do
       end
     end
   end
-  
+
   describe "invalid user" do
     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
