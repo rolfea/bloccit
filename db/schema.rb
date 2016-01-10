d66340d14b6dbeb28772385a5a88ec3727aec369
Alex Rolfe <rolfepercussion@gmail.com>
2016-01-09 22:48:57 -0600

    checkpoint43 completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109044322) do
+ActiveRecord::Schema.define(version: 20160110042455) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160109044322) do
     t.integer  "topic_id"
     t.integer  "user_id"
     t.integer  "rating"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
