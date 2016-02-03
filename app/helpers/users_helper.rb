a9ae463aad14bad9b2f7d2333e00876796ffec2f
Alex Rolfe <rolfepercussion@gmail.com>
2016-02-03 17:08:02 -0600

    Added profile features, comments listed w/ Gravatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def submission_count(number_of_submissions, type_of_submission)
+    if number_of_submissions == 0
+      "#{@user} has not submitted any #{type_of_submission} yet."
+    else
+      pluralize(@user.type_of_submission.count, type_of_submission)
+    end
+  end
 end
